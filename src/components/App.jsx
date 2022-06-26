f44c3cb9bd2597365cd9b9ee49c5197c598c0da6
Nikita Osadchyi <nikitaosadchyi7@gmail.com>
2022-06-26 18:33:45 +0300

    HomePage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
-import { lazy, Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
-import { Layout } from './Layout'
-import Review from "./AffitionalInfo/Reviews/Review";
-import Cast from "./AffitionalInfo/Cast/Cast";
+// import { lazy, Suspense } from "react";
+// import { Route, Routes } from "react-router-dom";
+// import { Layout } from './Layout'
+// import Review from "./AffitionalInfo/Reviews/Review";
+// import Cast from "./AffitionalInfo/Cast/Cast";
 
-const HomePage = lazy(() => 
-    import()
-)
+// const HomePage = lazy(() => 
+//     import()
+// )
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import api from "services/api";
+import MovieList from "components/MoviesList/MoviesList";
+
+
+export default function HomePage() {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        api.getTrendingMovies().then(data => {
+            data.results.map(({ id, title }) =>
+                setMovies(m => [...m, { id, title }])
+            );
+        })
+    }, []);
+
+    return (
+        <main>
+            <h1>Trending today</h1>
+            {movies.length !== 0 && <MovieList movies={movies}/>}
+        </main>
+    );
+}
